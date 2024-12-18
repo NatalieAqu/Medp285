e7081aa1228b2f58351780e1db23d63e47a0355c
NatalieAqu <natalie.aquino08@myhunter.cuny.edu>
2024-12-18 02:51:06 -0500

    Delete week14 directory

--- week14/parking-lot.js
+++
@@ -1,9 +0,0 @@
-// Add event listener to streetlights for toggling on and off
-const streetlights = document.querySelectorAll('.streetlight');
-
-streetlights.forEach(light => {
-    light.addEventListener('click', function() {
-        this.classList.toggle('on');
-        this.style.transition = "box-shadow 0.3s ease, background-color 0.3s ease";  // Adds a smooth transition
-    });
-});
